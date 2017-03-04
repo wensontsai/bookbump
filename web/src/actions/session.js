0c50214b05a2152632bfca5f445e5cdce6e9c254
Wenson Tsai <wensontsai@gmail.com>
2017-03-03 21:25:56 -0500

    moar.  moaaarr

---
+++ web/src/actions/session.js
@@ -0,0 +1,15 @@
+export function authenticate() {
+  return (dispatch) => {
+    dispatch({ type: 'AUTHENTICATION_REQUEST' });
+    return api.post('/sessions/refresh')
+      .then((response) => {
+        setCurrentUser(dispatch, response);
+      })
+      .catch(() => {
+        localStorage.removeItem('token');
+        window.location = '/login';
+      });
+  };
+}
+
+export const unauthenticate = () => ({ type: 'AUTHENTICATION_FAILURE' });
